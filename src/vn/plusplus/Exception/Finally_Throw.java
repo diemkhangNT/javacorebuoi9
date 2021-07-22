149ecf4442f58ae4d06819dc1df1d07ebd26b1c9
diemkhang1912@gmail.com <diemkhang1912@gmail.com>
2021-07-22 19:43:01 +0700

    Exeption

---
+++ src/vn/plusplus/Exception/Finally_Throw.java
@@ -0,0 +1,26 @@
+package vn.plusplus.Exception;
+
+public class Finally_Throw {
+    public static void main(String[] args) {
+        System.out.println("Before call phepChia func.");
+        int ret;
+        try{
+            ret = phepChia(23, 0);
+            System.out.println("Result = " + ret);
+        }catch (ArithmeticException e){
+            System.out.println("Có lỗi, dừng chương trình!");
+        }
+        System.out.println("After call phepChia func.");
+    }
+    private static int phepChia(int a, int b) {
+        System.out.println("phepChia func is being run!");
+        try {
+            int c = a / b;
+            return c;
+        } catch (ArithmeticException e) {
+            throw e;
+        } finally {
+            System.out.println("Finally block!");
+        }
+    }
+}
